c9bab15cf021882d911fd59635c7c3da5175f1a1
Benjaaaamín <benja_e_e@hotmail.com>
2024-09-30 10:36:28 -0300

    the last commit benja

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from flask import * # bad practice
 import pickle
 
+# ORM 
 app = Flask(__name__)
 
 # model = pickle.load(open('./model/model.pkl', 'rb'))
@@ -23,18 +24,6 @@ def newUser():
     print(data)  # Imprimir los datos recibidos
 
     return jsonify({"message": "Datos recibidos", "data": data}), 200
-
-
-@app.route("/predecir", methods=['POST'])
-def predict():
-    cuartos = int(request.form['cuartos'])
-    distancia = int(request.form['distancia'])
-    
-    predict = model.predict([[cuartos, distancia]])    
-    
-    output = round(predict[0], 2)
-
-    return render_template('index.html', predictText=f'La ksa con 4 cuartos tiene {predict}')
 
 if __name__ == '__main__':
     app.run()
